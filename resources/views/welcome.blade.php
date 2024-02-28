218a3aea4c445fdff4c886f5cc4255efa837af83
AntonSmetanin <anton.sm@mydevcube.com>
2024-02-29 03:25:04 +0600

    --init commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,150 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>G-Scrum</title>
+    <link rel="stylesheet" href="/css/front.css">
+</head>
+<body>
+    <header class="head">
+        <div class="section bg-white head-section">
+            <div class="row">
+                <div class="brick">
+                    <h2 class="title">Этапы работ</h2>
+                    <h3 class="subtitle">Подзаголовок</h3>
+                </div>
+                <div class="brick">
+                    <time datetime="12/16/2023 10:11" class="date">
+                        12/16/2023 10:11
+                    </time>
+                </div>
+            </div>
+        </div>
+    </header>
+    <main class="main-section">
+        <div class="section">
+            <nav class="tabs row">
+                <a href="#" class="tab-link">
+                    <strong>Name</strong>
+                    <em>Surname</em>
+                </a>
+                <a href="#" class="tab-link">
+                    <strong>Name</strong>
+                    <em>Surname</em>
+                </a>
+                <a href="#" class="tab-link">
+                    <strong>Name</strong>
+                    <em>Surname</em>
+                </a>
+            </nav>
+            <section class="content bg-white">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Название</th>
+                            <th>Срок выполнения</th>
+                            <th>Приоритет</th>
+                            <th>Статус</th>
+                            <th>Действия</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                             <td class="project-td-name">
+                                 <strong>Franmertics</strong>
+                             </td>
+                             <td>
+                                 <span>28 февраля </span> &mdash; <span>4 марта</span>
+                             </td>
+                             <td></td>
+                             <td></td>
+                             <td></td>
+                         </tr>
+                        <tr class="project-item-row">
+                            <td>
+                                <div class="table-td">
+                                    Выбор цветовой палитры
+                                </div>
+                            </td>
+                            <td><span>28 февраля </span> &mdash; <span>4 марта</span></td>
+                            <td>5</td>
+                            <td>В работе</td>
+                            <td>
+                                <button>Редактировать</button>
+                                <button>Удалить</button>
+                            </td>
+                        </tr>
+                        <tr>
+                             <td  class="project-td-name">
+                                 <strong>Coolframes</strong>
+                             </td>
+                             <td>
+                                 <span>28 февраля </span> &mdash; <span>4 марта</span>
+                             </td>
+                             <td></td>
+                             <td></td>
+                             <td></td>
+                         </tr>
+                        <tr class="project-item-row">
+                            <td>
+                                <div class="table-td">
+                                    Выбор цветовой палитры
+                                </div>
+                            </td>
+                            <td><span>28 февраля </span> &mdash; <span>4 марта</span></td>
+                            <td>5</td>
+                            <td>В работе</td>
+                            <td>
+                                <button>Редактировать</button>
+                                <button>Удалить</button>
+                            </td>
+                        </tr>
+                        <tr class="project-item-row">
+                            <td>
+                                <div class="table-td">
+                                    Выбор цветовой палитры
+                                </div>
+                            </td>
+                            <td><span>28 февраля </span> &mdash; <span>4 марта</span></td>
+                            <td>5</td>
+                            <td>В работе</td>
+                            <td>
+                                <button>Редактировать</button>
+                                <button>Удалить</button>
+                            </td>
+                        </tr>
+                         <tr>
+                             <td  class="project-td-name">
+                                 <strong>G-app</strong>
+                             </td>
+                             <td>
+                                 <span>28 февраля </span> &mdash; <span>4 марта</span>
+                             </td>
+                             <td></td>
+                             <td></td>
+                             <td></td>
+                         </tr>
+                         <tr class="project-item-row">
+                             <td>
+                                 <div class="table-td">
+                                     Выбор цветовой палитры
+                                 </div>
+                             </td>
+                             <td><span>28 февраля </span> &mdash; <span>4 марта</span></td>
+                             <td>5</td>
+                             <td>В работе</td>
+                             <td>
+                                 <button>Редактировать</button>
+                                 <button>Удалить</button>
+                             </td>
+                         </tr>
+                    </tbody>
+                </table>
+            </section>
+        </div>
+    </main>
+</body>
+</html>
